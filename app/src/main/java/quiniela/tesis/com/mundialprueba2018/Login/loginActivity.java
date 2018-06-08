1c699fa1d05b9b266c489349b8b0a96270a14d63
alfernando <alvaritoss@hotmail.com>
2018-06-07 22:05:54 -0600

    GET web Service

--- app/src/main/java/quiniela/tesis/com/mundialprueba2018/Login/loginActivity.java
+++ app/src/main/java/quiniela/tesis/com/mundialprueba2018/Login/loginActivity.java
@@ -4,14 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 
 import quiniela.tesis.com.mundialprueba2018.MainActivity;
 import quiniela.tesis.com.mundialprueba2018.R;
@@ -80,10 +96,59 @@ public class loginActivity extends Activity implements View.OnClickListener{
         else{
             Toast.makeText(getApplicationContext(),"Usuario Incorrecto",Toast.LENGTH_LONG).show();
             Intent intent = new Intent (this, MainActivity.class);
+            PL_GetUsuario();
             startActivity(intent);
             finish();
         }
 
     }
+
+    public void PL_GetUsuario(){
+        String sql = "https://rusiaquiniela2018.000webhostapp.com/Usuario.php";
+
+        StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        SSLServerSocketFactory.getDefault();
+
+        URL url = null;
+        HttpURLConnection conn;
+
+        try {
+            url = new URL(sql);
+            conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            String json ="";
+
+            while ((inputLine = in.readLine()) != null){
+                    response.append(inputLine);
+            }
+
+            json = response.toString();
+            JSONArray jsonArr = null;
+            jsonArr = new JSONArray(json);
+
+            for (int i = 0 ; i < jsonArr.length() ; i++){
+                JSONObject jsonObject = jsonArr.getJSONObject(i);
+                Log.d("SALIDA",jsonObject.optString("us_usuario"));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
 }
